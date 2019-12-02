b3ed2f21309c462ecd926926514c461f37635505
Leandro Takeda <letakeda@gmail.com>
2019-12-02 23:02:49 +0000

    A few changes to my react project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: ' by Jordan Walke'
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: ' by Dan Abramov, Andrew Clark'
+  }
+]
 
 class App extends Component {
   render() {
-    const welcome_message = 'Welcome to the road of learn React! Project: hackernews';
+    //const welcome_message = 'Welcome to the road of learn React!';
     //welcome_message = 'text'
     return (
       <div className="App">
-        <h2>{welcome_message}</h2>
+        {list.map(item =>
+
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a></span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+
+        )}
       </div>
     )
   }
